9fac61f2e8d943e4a92ea74ef101a3f30fdff1bd
Xiaopi0 <erikkirkegaardstage@gmail.com>
2020-10-21 12:40:42 +0200

    upload some scripts

---
+++ powershell downloadv2.ps1
@@ -0,0 +1,8 @@
+﻿$url = Read-Host -Prompt 'input link'
+$output = Read-Host -Prompt 'input destination(with file-name)'
+$start_time = Get-Date
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"

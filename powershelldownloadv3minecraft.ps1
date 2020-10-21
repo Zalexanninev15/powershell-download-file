71e5693b6c106346b941707b8b3b396a8a3e1e34
Xiaopi0 <erikkirkegaardstage@gmail.com>
2020-10-21 20:53:42 +0200

    Add files via upload

---
+++ powershelldownloadv3minecraft.ps1
@@ -0,0 +1,14 @@
+﻿$url = Read-Host -Prompt 'input link'
+$output = Read-Host -Prompt 'input destinationfolder'
+$name = Read-Host -Prompt 'input filename'
+$start_time = Get-Date
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+if($name -eq 'server.jar'){
+    cd $output
+    java -Xmx1024M -Xms1024M -jar server.jar nogui
+}

32d3c4cf87a695618f1efb332ad9dd1577128d04
Xiaopi0 <erikkirkegaardstage@gmail.com>
2020-10-22 18:04:42 +0200

    Update powershelldownloadv3minecraft.ps1

--- powershelldownloadv3minecraft.ps1
+++ powershelldownloadv3minecraft.ps1
@@ -1,4 +1,4 @@
-﻿$url = Read-Host -Prompt 'input link'
+$url = Read-Host -Prompt 'input link'
 $output = Read-Host -Prompt 'input destinationfolder'
 $name = Read-Host -Prompt 'input filename'
 $start_time = Get-Date
@@ -10,5 +10,11 @@ Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 if($name -eq 'server.jar'){
     cd $output
+    java -Xmx1024M -Xms1024M -jar server.jar nogui | Out-Null
+    $file = $output + '\eula.txt'
+    ((Get-Content -path $file -Raw) -replace 'false','true') | Set-Content -Path $file
+    $file2 = $output + '\Start.cmd'
+    New-Item -Path $file2 -ItemType File
+    Set-Content -Path $file2 -Value 'java -Xmx1024M -Xms1024M -jar server.jar nogui'
     java -Xmx1024M -Xms1024M -jar server.jar nogui
 }

b2954ae10d079da3d3b9014ab950ba509a111352
314ShadePi <erikkirkegaardstage@gmail.com>
2021-12-06 10:20:08 +0100

    MC1.18

--- minecraftrelated/get_a_normal_minecraft_server_easily - BitsTransfer.ps1
+++ minecraftrelated/get_a_normal_minecraft_server_easily - BitsTransfer.ps1
@@ -24,6 +24,7 @@ switch($version)
     '1.16.5'{$url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'}
     '1.17'{$url = 'https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar'}
     '1.17.1'{$url = 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar'}
+    '1.18'{$url = 'https://launcher.mojang.com/v1/objects/3cf24a8694aca6267883b17d934efacc5e44440d/server.jar'}
     default{Write-Output "You need to input a valid version, please restart the script to do so"}
 }
 
